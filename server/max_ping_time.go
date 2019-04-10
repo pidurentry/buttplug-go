0b2e068d769ac94f4750bc0b32df3acedaa394b3
pidurentry <pidur.entry@gmail.com>
2019-04-10 21:05:54 +0100

    Added a helper to the max ping time to return a Timer for use with sending the required ping messages to the server

--- server/max_ping_time.go
+++ server/max_ping_time.go
@@ -3,3 +3,11 @@ package server
 import "time"
 
 type MaxPingTime time.Duration
+
+func (ping MaxPingTime) Ticker() *time.Ticker {
+	pingTime := 5 * time.Second
+	if maxPingTime := time.Duration(ping); maxPingTime > 0 {
+		pingTime = maxPingTime
+	}
+	return time.NewTicker(pingTime)
+}
